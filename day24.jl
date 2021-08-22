8d1a58d4c7e45937b2cac809f6cd7eb40eb56d8f
Kunal Bhalla <bhalla.kunal@gmail.com>
2021-08-21 23:48:09 -0400

    Day 24

--- day24.jl
+++ day24.jl
@@ -1,32 +1,49 @@
-function bridge(components)
-    nodes = Dict{Int64, Set{Tuple{Int64, Int64}}}()
+function bridge2(components)
+    plugs_list = []
+    nodes_list = [[] for _ in 1:100]
     for component in components
         plugs = Tuple(map(x -> parse(Int64, x), split(component, "/")))
-        push!(get!(nodes, plugs[1], Set()), plugs)
-        push!(get!(nodes, plugs[2], Set()), plugs)
+        push!(plugs_list, plugs)
+        push!(nodes_list[plugs[1] + 1], (plugs[2], length(plugs_list)))
+        push!(nodes_list[plugs[2] + 1], (plugs[1], length(plugs_list)))
     end
 
-    bridge_helper(0, nodes)
+    used_list = [0 for _ in plugs_list]
+
+    bridge2_helper(plugs_list, nodes_list, used_list, 0)
 end
 
-function bridge_helper(root::Int64, nodes::Dict{Int64, Set{Tuple{Int64, Int64}}}) :: Int64
-    if !haskey(nodes, root) || isempty(nodes[root])
+function bridge2_helper(plugs_list, nodes_list, used_list, root)
+    if isempty(nodes_list[root + 1])
         return 0
     end
 
-    soln = nothing
+    max_strength = 0
+    longest = 0
+    longest_strength = 0
 
-    for choices in nodes[root]
-        other = choices[1] == root ? choices[2] : choices[1]
-        remaining_nodes = copy(nodes)
-        delete!(remaining_nodes[other], choices)
-        cur = root + other + bridge_helper(other, remaining_nodes)
-        if isnothing(soln) || soln < cur
-            soln = cur
+    for (next_root, plug) in nodes_list[root + 1]
+        if used_list[plug] != 0
+            continue
         end
+
+        used_list[plug] = 1
+        inner_problem = bridge2_helper(plugs_list, nodes_list, used_list, next_root)
+        cur_strength = root + next_root + inner_problem[1]
+        cur_length = 1 + inner_problem[2]
+        cur_longest_strength = root + next_root + inner_problem[3]
+
+        max_strength = max(max_strength, cur_strength)
+
+        if longest < cur_length || (longest == cur_length && longest_strength < cur_longest_strength)
+            longest = cur_length
+            longest_strength = cur_longest_strength
+        end
+
+        used_list[plug] = 0
     end
 
-    soln
+    max_strength, longest, longest_strength
 end
 
 function main()
@@ -40,8 +57,9 @@ function main()
 10/1
 9/10
 """), "\n")
-    # input = readlines()
-    println(bridge(input)[1])
+    input = readlines()
+    result = bridge2(input)
+    println(result[get(ARGS, 1, 1) == 1 ? 1 : 3])
 end
 
 main()
